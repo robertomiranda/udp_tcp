3866cd1495cddd915d8a2730c2f5c95f7276f932
unknown <roberto@.(none)>
2011-02-20 18:41:13 -0500

    adding service path

---
+++ UDP_TCP_BRIDGE/src/model/Service.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author roberto
+ */
+public class Service {
+
+    protected final static HashMap CLIENTS = new HashMap();
+
+    public Service(){
+    }
+
+}
